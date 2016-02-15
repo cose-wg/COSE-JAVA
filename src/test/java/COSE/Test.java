427ad168897fd18b494d8f1ac888c54f57452716
Joakim Brorsson <b.joakim@gmail.com>
2016-02-15 12:27:47 +0100

    Changed project structure for maven compability

--- src/test/java/COSE/Test.java
+++ src/test/java/COSE/Test.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package test;
+package COSE;
 
 import COSE.MAC0Message;
 import COSE.MACMessage;
